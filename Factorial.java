2df7473d09139aaec14db2a95b02bd1723c76cd5
Dhakshaya <107299048+Dhakshaya@users.noreply.github.com>
2022-06-11 15:54:03 +0530

    Add files via upload
    Dhakshayani

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Factorial {
+	public static void main (String[] args)
+	{
+	
+	int factorial =5;
+	int fact = 1;
+	for(int i=1;i<=factorial;i++)
+	{
+	fact = fact*i;
+	}
+	System.out.println(fact);
+	}
+	
+	
+
+}
